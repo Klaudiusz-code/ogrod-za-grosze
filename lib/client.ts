8d826455267db9373d450093881a703d19a148cb
Klaudiusz_dev <muminowy123@gmail.com>
2026-01-11 18:20:00 +0100

    lalaal

--- lib/client.ts
+++ lib/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const link = new HttpLink({
-  uri: "http://localhost/wordpress/graphql", 
+  uri: "https://vps-e1257184.vps.ovh.net/graphql", 
 });
 
 export const client = new ApolloClient({
