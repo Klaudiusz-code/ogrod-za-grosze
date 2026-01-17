ad25fc186e7cbe0ed511823f5e86e80cae980608
Klaudiusz_dev <muminowy123@gmail.com>
2026-01-17 20:41:34 +0100

    update

--- lib/client.ts
+++ lib/client.ts
@@ -2,6 +2,11 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const link = new HttpLink({
   uri: "https://www.ogrodzagrosze.pl/graphql", 
+  fetchOptions: {
+    next: {
+      revalidate: 1,
+    },
+  },
 });
 
 export const client = new ApolloClient({
